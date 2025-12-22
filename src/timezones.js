c4e3d75140e8269c2d59980f4ab0ffbf7b825043
Milan Mehra <milan@milanm.org>
2025-12-22 09:04:24 +0530

    update timezone database source, update timezone & airport data

--- src/timezones.js
+++ src/timezones.js
@@ -1,19 +1,36 @@
 const fs = require("fs");
 const axios = require("axios").default;
+const airports = require("../data/airports.json");
 
 axios
   .get(
-    "https://raw.githubusercontent.com/hroptatyr/dateutils/tzmaps/icao.tzmap"
+    "https://raw.githubusercontent.com/hroptatyr/dateutils/tzmaps/iata.tzmap"
   )
   .then(function (response) {
     let timezones = [];
     response.data.split("\n").forEach((line) => {
-      const [icao, tz] = line.split("\t");
-      if (icao == undefined) return;
+      const [iata, tz] = line.split("\t");
+      if (iata == undefined) return;
       if (tz == undefined) return;
-      timezones.push({ icao, tz });
+      timezones.push({ iata, tz });
     });
-    
+
+    let convertedAirports = [];
+    for (let i = 0; i < timezones.length; i++) {
+      let timezoneAirport = timezones[i];
+
+      const airport = airports.find((airport) => {
+        return airport.iata.toUpperCase() === timezoneAirport.iata;
+      });
+      if (!airport) continue;
+
+      convertedAirports.push({
+        iata: timezoneAirport.iata,
+        icao: airport.icao,
+        tz: timezoneAirport.tz,
+      });
+    }
+
     fs.writeFileSync(
       __dirname + "/../data/timezones.json",
       JSON.stringify(timezones)
