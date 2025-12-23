b4ddf8f551ec3a7bc136f787360efcfe2d543ee7
Milan Mehra <milan@milanm.org>
2025-12-23 16:23:36 +0530

    airports append, HJR appendation

--- src/airports.js
+++ src/airports.js
@@ -2,6 +2,8 @@ const csv = require("csvtojson");
 const fs = require("fs");
 const axios = require("axios").default;
 
+const append = require("./append/airports.json");
+
 console.log("Creating airports.json");
 axios
   .get("https://davidmegginson.github.io/ourairports-data/airports.csv", {
@@ -13,27 +15,59 @@ axios
     csv()
       .fromStream(stream)
       .then(function (jsonArrayObj) {
-        const filteredAirports = jsonArrayObj
-          .filter((row) => {
-            return (
-              row.iso_country &&
-              row.municipality &&
-              row.iata_code &&
-              row.icao_code &&
-              row.name &&
-              row.latitude_deg &&
-              row.longitude_deg
-            );
-          })
-          .map((row) => ({
-            country_code: row.iso_country,
-            region_name: row.municipality,
-            iata: row.iata_code,
-            icao: row.icao_code,
-            airport: row.name,
-            latitude: row.latitude_deg,
-            longitude: row.longitude_deg,
-          }));
+        let filteredAirports = [];
+        for (let n = 0; n < jsonArrayObj.length; n++) {
+          let row = jsonArrayObj[n];
+
+          let country_code = row.iso_country;
+          let region_name = row.municipality;
+          let iata = row.iata_code;
+          let icao = row.icao_code;
+          let airport = row.name;
+          let latitude = row.latitude_deg;
+          let longitude = row.longitude_deg;
+
+          for (let i = 0; i < append.length; i++) {
+            let appendElement = append[i];
+
+            if (appendElement.by === "iata") {
+              if (
+                appendElement.iata_code.toUpperCase() === iata.toUpperCase()
+              ) {
+                icao = appendElement.icao_code;
+                break;
+              }
+            } else if (appendElement.by === "icao") {
+              if (
+                appendElement.icao_code.toUpperCase() === icao.toUpperCase()
+              ) {
+                iata = appendElement.iata_code;
+                break;
+              }
+            }
+          }
+
+          if (
+            !country_code ||
+            !region_name ||
+            !iata ||
+            !icao ||
+            !airport ||
+            !latitude ||
+            !longitude
+          )
+            continue;
+
+          filteredAirports.push({
+            country_code,
+            region_name,
+            iata,
+            icao,
+            airport,
+            latitude,
+            longitude,
+          });
+        }
 
         console.log(
           `Filtered ${filteredAirports.length} airports from ${jsonArrayObj.length} total records`
